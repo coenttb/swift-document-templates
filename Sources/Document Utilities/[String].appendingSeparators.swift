b721871a9bb75191ce88b6c5da81237888662080
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-03-06 12:37:22 +0100

    added check to String.appendingSeparators method to not add separators if the last character is already that separator.

--- Sources/Document Utilities/[String].appendingSeparators.swift
+++ Sources/Document Utilities/[String].appendingSeparators.swift
@@ -15,20 +15,25 @@ extension [String] {
         atEnd lastSeparator: TranslatedString = ","
     ) -> [String] {
         guard !isEmpty else { return [] }
-        guard count > 1 else { return [self[0] + lastSeparator.description] }
+        guard count > 1 else {
+            let element = self[0]
+            return [element.hasSuffix(lastSeparator.description) ? element : element + lastSeparator.description]
+        }
         
         return enumerated().map { index, element in
             switch index {
             case count - 1:
-                return element + lastSeparator.description
+                return element.hasSuffix(lastSeparator.description) ? element : element + lastSeparator.description
             case count - 2:
                 if let secondLastSeparator = secondLastSeparator {
-                    return element + "\(separator) \(secondLastSeparator) "
+                    let secondLastSeparatorStr = "\(separator) \(secondLastSeparator) "
+                    return element.hasSuffix(secondLastSeparatorStr) ? element : element + secondLastSeparatorStr
                 } else {
-                    return element + separator.description
+                    return element.hasSuffix(separator.description) ? element : element + separator.description
                 }
             default:
-                return element + separator.description + " "
+                let standardSeparatorStr = separator.description + " "
+                return element.hasSuffix(standardSeparatorStr) ? element : element + standardSeparatorStr
             }
         }
     }
